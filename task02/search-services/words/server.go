3bd41d6167052cc5326008ef27d025ecd74c0e00
juliakononov <julickk04@mail.ru>
2026-05-02 17:14:31 +0300

    task2/words: implement search gRPC server

--- task02/search-services/words/server.go
+++ task02/search-services/words/server.go
@@ -3,14 +3,23 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
+	"fmt"
+	"log/slog"
 	"net"
 
+	"github.com/ilyakaznacheev/cleanenv"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 	wordspb "yadro.com/course/proto/words"
+	"yadro.com/course/words/words"
 )
+
+type Config struct {
+	Port int `yaml:"port" env:"WORDS_GRPC_PORT" env-default:"8080"`
+}
 
 type server struct {
 	wordspb.UnimplementedWordsServer
@@ -21,26 +30,47 @@ func (s *server) Ping(_ context.Context, in *emptypb.Empty) (*emptypb.Empty, err
 }
 
 func (s *server) Norm(_ context.Context, in *wordspb.WordsRequest) (*wordspb.WordsReply, error) {
+	if len(in.Phrase) > 4096 {
+		return nil, status.Errorf(codes.ResourceExhausted, "phrase exceeds 4096 bytes")
+	}
 	return &wordspb.WordsReply{
-		Words: []string{"I", "am", "not", "implemented"},
+		Words: words.Normalize(in.Phrase),
 	}, nil
 }
 
 func main() {
-	var address string
-	flag.StringVar(&address, "address", ":8080", "server address")
+	var configPath string
+	flag.StringVar(&configPath, "config", "", "path to config file")
 	flag.Parse()
 
-	listener, err := net.Listen("tcp", address)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	var cfg Config
+
+	if configPath != "" {
+		if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+			slog.Error("words: cannot read config", "error", err)
+			return
+		}
+	} else {
+		if err := cleanenv.ReadEnv(&cfg); err != nil {
+			slog.Error("words: cannot read env", "error", err)
+			return
+		}
 	}
+
+	addr := fmt.Sprintf(":%d", cfg.Port)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		slog.Error("words: failed to listen", "error", err)
+		return
+	}
+
+	slog.Info("words: starting server", "addr", addr)
 
 	s := grpc.NewServer()
 	wordspb.RegisterWordsServer(s, &server{})
 	reflection.Register(s)
 
 	if err := s.Serve(listener); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		slog.Error("words: failed to serve", "error", err)
 	}
 }
